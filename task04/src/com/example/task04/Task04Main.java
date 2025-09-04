eaf20060368d9149b0e4346e8effc407ed6b9f90
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 12:42:24 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,13 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point start = new Point(1, 1);
+        Point end = new Point(3, 5);
+        Point point = new Point(4, 7);
 
+        Line line = new Line(start, end);
+        boolean result = line.isCollinearLine(point);
+
+        System.out.println(result);
     }
 }
