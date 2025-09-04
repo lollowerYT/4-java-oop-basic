e94834fc01227f0d040228572e7f95428083aa46
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 12:50:28 +0500

    Update Line.java

--- task04/src/com/example/task04/Line.java
+++ task04/src/com/example/task04/Line.java
@@ -1,1 +1,27 @@
+package com.example.task04;
 
+class Line {
+    private final Point Start;
+    private final Point End;
+
+    public Line(Point start, Point end) {
+        Start = start;
+        End = end;
+    }
+
+    public Point getP1() {
+        return Start;
+    }
+
+    public Point getP2() {
+        return End;
+    }
+
+    public boolean isCollinearLine(Point p) {
+        return (End.x - Start.x) * (p.y - Start.y) - (p.x - Start.x) * (End.y - Start.y) == 0;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d), (%d, %d)", Start.x, Start.y, End.x, End.y);
+    }
+}
