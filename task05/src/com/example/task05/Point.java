1e8cd937fb852d51b7a683222ecd25afef436b96
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 13:02:55 +0500

    Update Point.java

--- task05/src/com/example/task05/Point.java
+++ task05/src/com/example/task05/Point.java
@@ -1,49 +1,26 @@
 package com.example.task05;
 
-/**
- * Точка в двумерном пространстве
- */
-public class Point {
+class Point {
+    private final double X;
+    private final double Y;
 
-    /**
-     * Конструктор, инициализирующий координаты точки
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
-    public Point(double x, double y) {
-        throw new AssertionError();
+    public Point(double x, double y){
+        X = x;
+        Y = y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
     public double getX() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return X;
     }
 
-    /**
-     * Возвращает координату точки по оси ординат
-     *
-     * @return координату точки по оси Y
-     */
     public double getY() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return Y;
     }
 
-    /**
-     * Подсчитывает расстояние от текущей точки до точки, переданной в качестве параметра
-     *
-     * @param point вторая точка отрезка
-     * @return расстояние от текущей точки до переданной
-     */
     public double getLength(Point point) {
-        // TODO: реализовать
-        throw new AssertionError();
-    }
+        double a = Math.pow(X - point.X, 2);
+        double b = Math.pow(Y - point.Y, 2);
 
+        return Math.sqrt(a + b);
+    }
 }
