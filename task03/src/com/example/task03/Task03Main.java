875a67051be07a96ddd2ae3aae85f9e51cfadd90
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 12:34:02 +0500

    Update Task03Main.java

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,7 +2,12 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber num1 = new ComplexNumber(5, 4);
+        ComplexNumber num2 = new ComplexNumber(10, 6);
+        ComplexNumber sum = num1.add(num2);
+        ComplexNumber mul = num1.multiply(num2);
+        System.out.println(sum);
+        System.out.println(mul);
     }
 }
 
