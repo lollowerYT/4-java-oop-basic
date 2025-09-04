c2a7b8e979538ea736bec522d33ac5495f209e1a
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 13:07:32 +0500

    Update Task05Main.java

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,13 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
+        PolygonalLine line = new PolygonalLine();
+        line.addPoint(0, 0);
+        line.addPoint(3, 4);
+        line.addPoint(6, 0);
+
+        System.out.println(line);
+        System.out.println("Длина ломаной: " + line.getLength());
 
     }
 }
