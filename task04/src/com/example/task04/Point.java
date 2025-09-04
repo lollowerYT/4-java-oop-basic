f87b069594c9afce0bec389fd63d9dc8d3bd2ac3
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 12:43:27 +0500

    Update Point.java

--- task04/src/com/example/task04/Point.java
+++ task04/src/com/example/task04/Point.java
@@ -1,1 +1,22 @@
+package com.example.task04;
 
+class Point {
+    final int x;
+    final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        double a = Math.pow(x - point.x, 2);
+        double b = Math.pow(y - point.y, 2);
+
+        return Math.sqrt(a + b);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+}
