57f3994214c2b0c27ef10c02f47e02a766605bf7
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-09-04 12:41:31 +0500

    Update Task03Main.java

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -10,29 +10,3 @@ public class Task03Main {
         System.out.println(mul);
     }
 }
-
-class ComplexNumber {
-    private double real;
-    private double imaginary;
-
-    public ComplexNumber(double real, double imaginary) {
-        this.real = real;
-        this.imaginary = imaginary;
-    }
-
-    public ComplexNumber add(ComplexNumber other) {
-        double newReal = this.real + other.real;
-        double newImaginary = this.imaginary + other.imaginary;
-        return new ComplexNumber(newReal, newImaginary);
-    }
-
-    public ComplexNumber multiply(ComplexNumber other) {
-        double newReal = this.real * other.real - this.imaginary * other.imaginary;
-        double newImaginary = this.real * other.imaginary + this.imaginary * other.real;
-        return new ComplexNumber(newReal, newImaginary);
-    }
-
-    public String toString() {
-        return String.format("%.2f + %.2fi", real, imaginary);
-    }
-}
